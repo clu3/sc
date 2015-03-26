cf2d37f954225d4590c747cef56498c254c9d073
Steve Tran <steve.tran.38@gmail.com>
2015-03-26 10:15:05 +0700

    Sc phien ban 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Parse Package Scorm</title>
+	<!--Khai b�o s? d?ng boostrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<!--S?a l?i HTML5 cho IE 8 tr? xu?ng-->
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+</head>
+<body>
+	<form enctype="multipart/form-data" method="post" action="upload.php">
+		<label>Choose a zip file to upload: <input type="file" name="zip_file" /></label>
+		<br />
+	<input type="submit" name="submit" value="Upload" />
+	</form>
+    <!-- Khai b�o thu vi?n jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Khai b�o s? d?ng thu vi?n javascript c?a bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
